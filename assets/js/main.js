9a455051c4bd22772d048823a913af0eca397336
Ian Melo <95489893+ichumbo@users.noreply.github.com>
2023-02-08 13:56:37 -0300

    criação do main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
